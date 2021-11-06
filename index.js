8fdd465470182b28d367cdde28a3f0e39050f289
joydip10 <joydipdasNSTU@gmail.com>
2021-11-07 00:45:03 +0600

    second commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wuxif.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const port = 500;
+const port = process.env.PORT || 500;
 
 app.get('/', (req, res) => {
     res.send('CRUD served');
